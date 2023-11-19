71ad2fad882182abd8b389b76e28f8b639bb98fc
rizalpahlevii <mrizalpahlevi372@gmail.com>
2023-11-19 15:29:05 +0700

    Updating database config

--- models/setup.go
+++ models/setup.go
@@ -13,7 +13,7 @@ func ConnectDatabase() {
 	password := "d-c42b5d-ABea*4a-fBBCDA*23E1AAe-"
 	dbname := "railway"
 	port := "45094"
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Shanghai", host, port, user, password, dbname)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Jakarta", host, user, password, dbname, port)
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
